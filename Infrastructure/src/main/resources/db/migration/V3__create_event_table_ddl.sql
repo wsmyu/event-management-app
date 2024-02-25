008461a053c8a26ebd4bea726605a891266212fd
Wing Suet Michelle Yu <n01535631@humbermail.ca>
2024-02-25 11:07:16 -0500

    modify v2 script

--- Infrastructure/src/main/resources/db/migration/V3__create_event_table_ddl.sql
+++ Infrastructure/src/main/resources/db/migration/V3__create_event_table_ddl.sql
@@ -1,11 +1,12 @@
 CREATE TABLE events (
-    event_id BIGINT  PRIMARY KEY AUTO_INCREMENT,
-    user_id BIGINT  NOT NULL,
-    venue_id BIGINT  NOT NULL,
+    event_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    venue_id BIGINT NOT NULL,
     event_name VARCHAR(255),
     event_type VARCHAR(255),
     event_date DATE,
-    event_time TIME,
+    event_start_time TIME,
+    event_end_time TIME,
     event_description TEXT,
     event_budget DECIMAL(10,2),
     status VARCHAR(50),
@@ -13,4 +14,4 @@ CREATE TABLE events (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (user_id),
     FOREIGN KEY (venue_id) REFERENCES venues (venue_id)
- );
+);
